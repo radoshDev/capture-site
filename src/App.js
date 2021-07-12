617b25707f7242e129ed48b479443f66d07c6e5a
radoshDev <radosh.dev@gmail.com>
2021-07-12 11:34:25 +0300

    Add markup for about section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import AboutUs from "./pages/AboutUs";
 
 const App = () => {
 	return (
-		<div>
-			<h1>Hello react</h1>
+		<div className="App">
+			<AboutUs />
 		</div>
 	);
 };
